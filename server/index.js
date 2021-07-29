cf3ca294fb4265844d7d1a87834bfcafff4f3394
Hai Nghiem <hainghiem375@gmail.com>
2021-07-29 17:01:15 -0230

    connect to MongoDB Atlas

--- server/index.js
+++ server/index.js
@@ -1,3 +1,18 @@
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
+require('dotenv').config();
 
+const app = express();
+
+//Setup database credentials
+const mongodbPassword = process.env.MONGODB_PASSWORD;
+const connectionString = `mongodb+srv://hainghiem:${mongodbPassword}@coffeeshop-tracker.kmuzu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+//Connect to database
+mongoose.connect(connectionString, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+.then(()=> console.log(`Connected to MongoDB Atlas!`))
+.catch(err => console.log(err));
