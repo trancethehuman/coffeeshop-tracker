89e4bd7e4ee21c4854ff282b8ed030e4c6609efc
Hai Nghiem <hainghiem375@gmail.com>
2021-08-18 16:24:45 -0230

    working demo

--- index.js
+++ index.js
@@ -1,40 +1,25 @@
-// Import dependencies
-const fs = require("fs");
-const PDFDocument = require("./pdfkit-tables");
+const generatePDF = require('./services/generatePDF');
+const pdf2json = require('./services/pdf2json');
+const prompt = require('prompt-sync')();
 
-// Load JSON
-const sourceJSON = require("./patients.json");
-
-// Create The PDF document
-const doc = new PDFDocument();
-
-// Pipe the PDF into a new PDF file
-doc.pipe(fs.createWriteStream(`patients.pdf`));
-
-// Add the header - https://pspdfkit.com/blog/2019/generate-invoices-pdfkit-node/
-doc
-    .image("logo.png", 50, 45, { width: 50 })
-    .fillColor("#444444")
-    .fontSize(20)
-    .text("Patient Information.", 110, 57)
-    .fontSize(10)
-    .text("725 Fowler Avenue", 200, 65, { align: "right" })
-    .text("Chamblee, GA 30341", 200, 80, { align: "right" })
-    .moveDown();
-
-// Create the table - https://www.andronio.me/2017/09/02/pdfkit-tables/
-const table = {
-    headers: ["Name", "Address", "Phone", "Birthday", "Email Address", "Blood Type", "Height", "Weight"],
-    rows: []
-};
-
-// Add the patients to the table
-for (const patient of sourceJSON) {
-    table.rows.push([patient.name, patient.address, patient.phone, patient.birthday, patient.emailAddress, patient.bloodType, patient.height, patient.weight])
+const parsePDFWithPrompt = (sourcePDFPath) => {
+    const input = prompt(`Parse PDF source and out a JSON file? y/n `);
+    if (input == `y` || `yes`) {
+        pdf2json(sourcePDFPath);
+    }
 }
 
-// Draw the table
-doc.moveDown().table(table, 10, 125, { width: 590 });
+const generatePDFWithPrompt = (sourceJSON) => {
+    const input = prompt(`Generate PDF from JSON source? y/n `);
+    if (input == `y` || `yes`) {
+        generatePDF(sourceJSON);
+    }
+}
 
-// Finalize the PDF and end the stream
-doc.end();
+const sourcePDFPath = `./testData/sourcePDF.pdf`;
+const sourceJSON = require('./testData/patients.json');
+
+parsePDFWithPrompt(sourcePDFPath);
+generatePDFWithPrompt(sourceJSON);
+
+
