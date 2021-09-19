4038fb66a3ee03428a182074b4fee09662b3ce3c
Hai Nghiem <hainghiem375@gmail.com>
2021-09-19 14:29:17 -0230

    added bootstrap

--- packages/client/src/components/ShopCardsContainer.js
+++ packages/client/src/components/ShopCardsContainer.js
@@ -1,11 +1,35 @@
-import Card from 'react-bootstrap';
+import Card from 'react-bootstrap/Card';
 
 // Organize coffeeshop cards into a scrollable box
 const ShopCardsContainer = () => {
     return (
-        <div>
-            <h3>Shop Cards Container!</h3>
-        </div>
+[
+  'Primary',
+  'Secondary',
+  'Success',
+  'Danger',
+  'Warning',
+  'Info',
+  'Light',
+  'Dark',
+].map((variant, idx) => (
+  <Card
+    bg={variant.toLowerCase()}
+    key={idx}
+    text={variant.toLowerCase() === 'light' ? 'dark' : 'white'}
+    style={{ width: '18rem' }}
+    className="mb-2"
+  >
+    <Card.Header>Header</Card.Header>
+    <Card.Body>
+      <Card.Title>{variant} Card Title </Card.Title>
+      <Card.Text>
+        Some quick example text to build on the card title and make up the bulk
+        of the card's content.
+      </Card.Text>
+    </Card.Body>
+  </Card>
+))
     )
 }
 
